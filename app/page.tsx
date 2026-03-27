26d53062d3b0c5593051fbd4a82eae63427d0b8a
ApirakGGG <naratah012345@gmail.com>
2026-03-27 22:18:37 +0700

    แก้homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,15 @@
 import { auth } from "@clerk/nextjs/server";
-import { PrismaClient } from "@/lib/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { redirect } from "next/navigation";
 import Dashboard from "./dashboard/page";
-
-const prisma = new PrismaClient({
-  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL })
-});
 
 export default async function Page() {
   const { userId } = await auth(); // clerkId
 
   if (!userId) {
-    redirect("/sign-in")
+    redirect("/sign-in");
   }
-
-  // ค้นหาข้อมูลผู้ใช้ใน Supabase ที่ตรงกับ clerkId 
-  const dbUser = await prisma.user.findUnique({
-    where: { clerkId: userId },
-    include: { interviews: true } // ดึงข้อมูลการสัมภาษณ์มาด้วย
-  });
-
   return (
     <div className="mx-auto">
-      {/* <h1>สวัสดีคุณ {dbUser?.name}</h1>
-      <p>คุณสัมภาษณ์ไปแล้ว {dbUser?.interviews.length} ครั้ง</p> */}
       <main className="p-4">
         <Dashboard />
       </main>
