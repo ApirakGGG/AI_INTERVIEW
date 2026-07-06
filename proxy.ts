55fdc636b91959f3a2650492aab43b8042770bf5
ApirakGGG <naratah012345@gmail.com>
2026-07-06 18:54:39 +0700

    แก้middle ware เพื่อทดสอบ API

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// สร้าง Matcher สำหรับเส้นทางที่เป็น Public (ไม่ต้อง Log in)
+// Matcher ที่เป็น Public เวลาtest postman จะไม่ต้อง Log in
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)', 
   '/sign-up(.*)', 
-  '/api/webhooks(.*)' // ✅ เพิ่มบรรทัดนี้เพื่อให้ Webhook ใช้งานได้
+  '/api/webhooks(.*)', 
+  '/api/getUsers(.*)',
+  '/api/summary(.*)',
+  '/api/deleteInterview(.*)',
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
